b86a5d71590d625f8c6ff88006bdb15ac5353d02
Duman96 <duman.iklassov@nu.edu.kz>
2018-11-20 17:51:09 +0600

    SE

--- src/main/java/com/programmerscuriosity/MyResource.java
+++ src/main/java/com/programmerscuriosity/MyResource.java
@@ -1,13 +1,27 @@
 package com.programmerscuriosity;
 
+import org.glassfish.jersey.internal.util.Base64;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created by marom on 27/09/16.
@@ -21,10 +35,15 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
+
+
+
+
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("{id}")
-    public Response getIt(@PathParam("id") String id) {
+    public String getIt(@PathParam("id") String id) throws IOException, SQLException {
         MySqlConnect mysqlConnect = new MySqlConnect();
         System.out.println("ID is " + id);
         System.out.println("Type of id " + ((Object) id).getClass().getName());
@@ -32,25 +51,38 @@ public class MyResource {
         String sql = "select * from professors where ID = " + foo;
         String result_n = null;
         String result_s = null;
-        InputStream binaryStream = null;
+        String result_school = null;
+        String result_dep = null;
+        String result_pos= null;
+        String result_bio = null;
+        Blob b = null;
+
+        ResultSet rs = null;
         try {
 //            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
 //            ResultSet rs = statement.executeQuery();
 
             Connection conn = mysqlConnect.connect();
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
+            rs = stmt.executeQuery(sql);
             System.out.println(rs.toString());
 
             while (rs.next()) {
-                String sid = rs.getString(2);
-                String lid = rs.getString(3);
+                result_n = rs.getString(2);
+                result_s = rs.getString(3);
+                result_school = rs.getString(4);
+                result_dep = rs.getString(5);
+                result_pos = rs.getString(6);
+                result_bio = rs.getString(7);
                 // Do whatever you want to do with these 2 values
 
-                result_n = sid;
-                result_s = lid;
                 //System.out.println("Result is: " + result);
-                binaryStream = rs.getBinaryStream(9);
+                b = rs.getBlob(9);
+                System.out.println("BLOB IS: \n");
+                //System.out.println(Arrays.toString(blob));
+
+                //photo64 = new String(b.getBytes(1l, (int) b.length()));
+
 
             }
 
@@ -59,8 +91,9 @@ public class MyResource {
         } finally {
             mysqlConnect.disconnect();
         }
-
-        return Response.status(200).entity("<!DOCTYPE html>\n" +
+        String imgsrc = "data:image/jpg;base64," + b;
+        String str = "Dana";
+        String html = "<!DOCTYPE html>\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "<head>\n" +
                 "    <meta charset=\"utf-8\" />\n" +
@@ -71,23 +104,13 @@ public class MyResource {
                 "        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">\n" +
                 "        <![endif]-->\n" +
                 "    <title>Just App - Landing Page</title>\n" +
-                "    <!-- BOOTSTRAP CORE CSS -->\n" +
-                "    <link href=\"assets/css/bootstrap.css\" rel=\"stylesheet\" />\n" +
-                "    <!-- ION ICONS STYLES -->\n" +
-                "    <link href=\"assets/css/ionicons.css\" rel=\"stylesheet\" />\n" +
-                "    <!-- CUSTOM CSS -->\n" +
-                "    <link href=\"assets/css/style.css\" rel=\"stylesheet\" />\n" +
-                "    <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->\n" +
-                "    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\n" +
-                "    <!--[if lt IE 9]>\n" +
+                "    <link href=\"http://localhost:8080/assets/css/bootstrap.css\" rel=\"stylesheet\" />\n" +
+                "    <link href=\"http://localhost:8080/assets/css/ionicons.css\" rel=\"stylesheet\" />\n" +
+                "    <link href=\"http://localhost:8080/assets/css/style.css\" rel=\"stylesheet\" />\n" +
                 "        <script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>\n" +
                 "        <script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script>\n" +
-                "    <![endif]-->\n" +
                 "</head>\n" +
-                "<body>\n" +
-                "\n" +
                 "   <div class=\"navbar navbar-default navbar-fixed-top scroll-me\">\n" +
-                "        <!-- pass scroll-me class above a tags to starts scrolling -->\n" +
                 "        <div class=\"container\">\n" +
                 "            <div class=\"navbar-header\">\n" +
                 "                <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">\n" +
@@ -95,92 +118,57 @@ public class MyResource {
                 "                    <span class=\"icon-bar\"></span>\n" +
                 "                    <span class=\"icon-bar\"></span>\n" +
                 "                </button>\n" +
-                "                <a class=\"navbar-brand\" href=\"index.jsp\">\n" +
+                "                <a class=\"navbar-brand\" href=\"http://localhost:8080/index.jsp\">\n" +
                 "                 <strong>NULL</strong>   \n" +
                 "                </a>\n" +
-                "\n" +
                 "            </div>\n" +
                 "            <div class=\"navbar-collapse collapse\">\n" +
                 "                <ul class=\"nav navbar-nav navbar-right\">\n" +
-                "\n" +
-                "                    <li><a class=\"nav-bar-a\"href=\"profs.jsp\">PROFESSOR</a></li>\n" +
+                "                    <li><a class=\"nav-bar-a\"href=\"http://localhost:8080/profs.jsp\">PROFESSOR</a></li>\n" +
                 "                    <li><a class=\"nav-bar-a\"href=\"#features\">COURSES</a></li>\n" +
-                "                    <li><a class=\"nav-bar-a\"href=\"about_us.jsp\">ABOUT US</a></li>\n" +
-                "                    <li><a class=\"nav-bar-user-a\" href=\"profile_s.jsp\">PROFILE</a></li>\n" +
-                "                    <li><a class=\"nav-bar-user-a\" href=\"logout\">LOG OUT</a></li>\n" +
+                "                    <li><a class=\"nav-bar-a\"href=\"http://localhost:8080/about_us.jsp\">ABOUT US</a></li>\n" +
+                "                    <li><a class=\"nav-bar-user-a\" href=\"http://localhost:8080/profile_s.jsp\">PROFILE</a></li>\n" +
+                "                    <li><a class=\"nav-bar-user-a\" href=\"http://localhost:8080/logout\">LOG OUT</a></li>\n" +
                 "                </ul>\n" +
                 "            </div>\n" +
-                "\n" +
                 "        </div>\n" +
                 "    </div>\n" +
                 "<section id=\"about\">\n" +
                 "        <div class=\"container\">\n" +
-                "\n" +
-                "\n" +
                 "            <div class=\"row scroll-me\">\n" +
                 "                <div class=\"col-md-4\">\n" +
-                "                    <h4>"+result_n + " " + result_s +"</h4>\n" +
-                "                    <h5>Assistant Proffesor</h5>\n" +
-                "\n" +
-                "                   <img src=\"assets/img/mark_sterling.jpeg\" class=\"prof_img\">\n" +
-                "                   <h5>School of Science and Technology</h5>\n" +
-                "                    <h5>Computer Science</h5>\n" +
-                "                        <div style=\"margin-left:-20px;\"class=\"col-md-10 col-10 col-lg-10 col-xl-10\"> \n" +
-                "                            <a href=\"#subscribe\" data-toggle=\"modal\" data-target=\".bd-example-modal-sm-msg\"  class=\"btn btn-custom btn-two profile_btn\"><b class=\"avatar_type_1\"><img src=\"assets/img/message.png\">Write a Message</b></a>\n" +
-                "                        </div>\n" +
+                "                    <h4>"+ result_n + " " + result_s + "</h4>\n" +
+                "                    <h5>" + result_pos + "</h5>\n" +
+                "                    <img src=\"http://localhost:8080/assets/profpic/" + id +".jpg\"" + " class=\"prof_img\">\n" +
+                "                    <h5>" + result_school + "</h5>\n" +
+                "                    <h5>" + result_dep + "</h5>\n" +
+                "                          <div style=\"margin-left:-20px;\"class=\"col-md-10 col-10 col-lg-10 col-xl-10\"> \n" +
+                "                            <a href=\"#subscribe\" data-toggle=\"modal\" data-target=\".bd-example-modal-sm-msg\"  class=\"btn btn-custom btn-two profile_btn\"><b class=\"avatar_type_1\">\n" +
+                "                                <img src=\"http://localhost:8080/assets/img/message.png\">Write a Message</b></a>\n" +
+                "                          </div>\n" +
                 "                </div>\n" +
                 "                <div class=\"col-md-4\">\n" +
                 "                    <h4>Bio</h4>\n" +
                 "                    <p>\n" +
-                "                    <strong> "+result_n + " " + result_s +"</strong>  received the B.S. degree in Electrical and Computer Engineering from the\n" +
-                "University of Rochester, Rochester, NY in 2002 and the M.S. and Ph.D. degrees in Electrical and Computer\n" +
-                "Engineering from the University of Rochester, Rochester NY, in 2004 and 2010 respectively. He held post-\n" +
-                "doctoral positions at the University of Rochester and Rochester Institute of Technology from 2010 to 2014.\n" +
-                "He is currently Assistant Professor in the Department of Computer Science, School of Science and\n" +
-                "Technology, Nazarbayev University and has published articles in IEEE Transactions and served on technical\n" +
-                "committees of international conferences. His research interests include biomedical and audio signal\n" +
-                "processing as applied to translational medicine and mobile sensing.\n" +
+                                        result_bio +
                 "                    </p>\n" +
                 "                </div>\n" +
                 "            </div>\n" +
+                "            <br>\n" +
                 "        </div>\n" +
-                "    </section>\n" +
-                "   \n" +
+                "</section>\n" +
                 "    <div class=\"footer\">\n" +
                 "        <center>\n" +
-                "                    <b>Designed by NULL TM </b> <br>\n" +
-                "                \n" +
-                "        </center>\n" +
-                "\n" +
-                "    </div>\n" +
-                "\n" +
-                "<div class=\"modal fade bd-example-modal-sm-msg\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" aria-hidden=\"true\">\n" +
-                "  <div class=\"modal-dialog modal-sm\">\n" +
-                "    <div class=\"modal-content\">\n" +
-                "        <center>\n" +
-                "      <h2> Message</h2><hr>\n" +
-                "     \n" +
-                "             <input class=\"msg_input\" placeholder=\"Message\" type=\"text\" name=\"password\" /><br />\n" +
-                "             <hr>\n" +
-                "            <input type=\"submit\" class=\"submit-sign\"  name=\"submit\" value=\"SUBMIT\">\n" +
-                "    </form>\n" +
+                "                    <b>Designed by NULL TM </b> <br>       \n" +
                 "        </center>\n" +
                 "    </div>\n" +
-                "  </div>\n" +
-                "</div>\n" +
-                "\n" +
-                "\n" +
-                "    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->\n" +
-                "    <!-- CORE JQUERY  SCRIPTS -->\n" +
-                "    <script src=\"assets/js/jquery-1.11.1.js\"></script>\n" +
-                "    <!-- BOOTSTRAP SCRIPTS  -->\n" +
-                "    <script src=\"assets/js/bootstrap.js\"></script>\n" +
-                "    <!-- SCROLLING SCRIPTS PLUGIN  -->\n" +
-                "    <script src=\"assets/js/jquery.easing.min.js\"></script>\n" +
-                "    <!-- CUSTOM SCRIPTS   -->\n" +
-                "    <script src=\"assets/js/custom.js\"></script>\n" +
+                "    <script src=\"http://localhost:8080/assets/js/jquery-1.11.1.js\"></script>\n" +
+                "    <script src=\"http://localhost:8080/assets/js/bootstrap.js\"></script>\n" +
+                "    <script src=\"http://localhost:8080/assets/js/jquery.easing.min.js\"></script>\n" +
+                "    <script src=\"http://localhost:8080/assets/js/custom.js\"></script>\n" +
                 "</body>\n" +
-                "</html>\n")
-                .build();
+                "</html>\n";
+        return html;
+
     }
 }
--- src/main/java/webapp/Search.java
+++ src/main/java/webapp/Search.java
@@ -41,11 +41,45 @@ public class Search extends HttpServlet {
             st = conn.createStatement();
             rs = st.executeQuery(sql);
 
-            out.println("<html>");
+            out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
             out.println("<head>");
-            out.println("<title>Hola</title>");
+            out.println("<meta charset=\"utf-8\" />");
+            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\" />");
+            out.println("<meta name=\"description\" content=\"\" />");
+            out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">");
+            out.println("<title>Just App - Landing Page</title>");
+            out.println("<link href=\"http://localhost:8080/assets/css/bootstrap.css\" rel=\"stylesheet\" />");
+            out.println("<link href=\"http://localhost:8080/assets/css/ionicons.css\" rel=\"stylesheet\" />");
+            out.println("<link href=\"http://localhost:8080/assets/css/style.css\" rel=\"stylesheet\" />");
+            out.println("<script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>");
+            out.println("<script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script>");
             out.println("</head>");
             out.println("<body bgcolor=\"white\">");
+            out.println("<div class=\"navbar navbar-default navbar-fixed-top scroll-me\">");
+            out.println("<div class=\"container\">");
+            out.println("<div class=\"navbar-header\">");
+            out.println("<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">");
+            out.println("<span class=\"icon-bar\"></span>");
+            out.println("<span class=\"icon-bar\"></span>");
+            out.println("<span class=\"icon-bar\"></span>");
+            out.println("</button>");
+            out.println("<a class=\"navbar-brand\" href=\"http://localhost:8080/index.jsp\"> <strong>NULL</strong> </a>");
+            out.println("</div>");
+            out.println("<div class=\"navbar-collapse collapse\">");
+            out.println("<ul class=\"nav navbar-nav navbar-right\">");
+            out.println("<li><a class=\"nav-bar-a\"href=\"http://localhost:8080/profs.jsp\">PROFESSOR</a></li>\n" +
+                    "                <li><a class=\"nav-bar-a\"href=\"#features\">COURSES</a></li>\n" +
+                    "                <li><a class=\"nav-bar-a\"href=\"http://localhost:8080/about_us.jsp\">ABOUT US</a></li>\n" +
+                    "                <li><a class=\"nav-bar-user-a\" href=\"http://localhost:8080/profile_s.jsp\">PROFILE</a></li>\n" +
+                    "                <li><a class=\"nav-bar-user-a\" href=\"http://localhost:8080/logout\">LOG OUT</a></li>");
+            out.println("</ul> </div> </div> </div>");
+
+            out.println("");
+            out.println("");
+            out.println("");
+
+            out.println("<title>Hola</title>");
+
 
 
             int count = 0;
@@ -57,6 +91,10 @@ public class Search extends HttpServlet {
                 String surname = rs.getString(3);
 
                 ns = name + " " + surname;
+                String pos = rs.getString(6);
+                String school = rs.getString(4);
+                String dept = rs.getString(5);
+
 
 //                out.print(x+" ");
 //                x = rs.getString(4);
@@ -74,14 +112,37 @@ public class Search extends HttpServlet {
 //                if(x != null) out.println(x);
 //                else out.println("No email yet");
 //                out.println("******************************************************************************************************************");
-
+                String imgsrc = "data:image/jpg;base64,";
                 String ref = "http://localhost:8080/rest/professors/" + id;
-                out.println("<h1> TOTAL NAME IS: <a href = " + ref + ">" + ns + "</a></h1> ");
-                out.println("</body>");
-                out.println("</html>");
-                System.out.println(count);
-            }
+                //out.println("<h1> TOTAL NAME IS: <a href = " + ref + ">" + ns + "</a></h1> ");
+                out.println("<section id=\"about\">\n" +
+                        "    <div class=\"container\">\n" +
+                        "        <div class=\"row scroll-me\">\n" +
+                        "            <center>\n" +
+                        "                <div class=\"col-md-12\">\n" +
+                        "                    <h1><a href = " + ref + ">" + ns + "</a></h1>\n" +
+                                            "<h2>" + pos + ", " + dept + ", " + school + "</h2>\n" +
+                                            //"<img src=\"http://localhost:8080/assets/profpic/" + id + ".jpg\"" + " class=\"prof_img\">" +
+                        "                </div>\n" +
+                        "            </center>\n" +
+                        "        </div><br>\n" +
+                        "    </div>\n" +
+                        "</section>");
 
+
+            }
+            out.println("<div class=\"footer\">\n" +
+                    "    <center>\n" +
+                    "        <b>Designed by NULL TM </b> <br>\n" +
+                    "    </center>\n" +
+                    "</div>\n" +
+                    "<script src=\"assets/js/jquery-1.11.1.js\"></script>\n" +
+                    "<script src=\"assets/js/bootstrap.js\"></script>\n" +
+                    "<script src=\"assets/js/jquery.easing.min.js\"></script>\n" +
+                    "<script src=\"assets/js/custom.js\"></script>\n" +
+                    "</body>\n" +
+                    "</html>");
+            System.out.println(count);
 
 
         }catch (ClassNotFoundException ex){
